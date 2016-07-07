f42288650dc5ad5483d99a306f40f21c1020cfeb
Jose Hernandez <josehernandez@hsfranchise.com>
2016-07-07 16:18:40 -0300

    First upload

---
+++ AngularGOD/app/app.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="D:\Work\VSWorkspace\AngularGOD\AngularGOD\Scripts/angular.min.js" />
+
+var godApp = angular.module('rootModule', ['ui.bootstrap', 'ui.router']);
+godApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise("/start");
+
+    $stateProvider
+                .state('Start', {
+                    url: '/start',
+                    views: {
+                        'main': {
+                            templateUrl: '/app/views/game/start.html'
+                        }
+                    }
+                });
+}]).
+run();
